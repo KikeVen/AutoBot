04610c3c23b38e8939c28036bcb769518dba3990
Enrique B <bruzual.kike@gmail.com>
2020-03-26 19:04:20 -0400

    refactor

--- autobot_logic.py
+++ autobot_logic.py
@@ -13,7 +13,8 @@ ANSWERS = os.path.join(THIS_FOLDER, 'answers.txt')
 
 def random_greet():
     """ Returns an item from random_list chosen randomly """
-    random_list = ['Hi', 'Hi there', 'Hello', 'Here we go', 'I got it', 'Ok', 'Well']
+    random_list = ['Hi', 'Hi there', 'Hello',
+                   'Here we go', 'I got it', 'Ok', 'Well']
     return random.choice(random_list)
 
 
@@ -33,29 +34,24 @@ def answers(answer_this):
         answers_dict[keyword] = [description, link]
     answers_csv.close()
 
-    the_one = []
+    finalists = []
     for k in answers_dict.keys():
         if bool(set(k.split()) & set(answer_this)):
-            the_one.append((k, answers_dict[k][0], answers_dict[k][1]))
+            finalists.append((k, answers_dict[k][0], answers_dict[k][1]))
 
-    if len(the_one) == 0:
+    if len(finalists) == 0:
         message = (f"Sorry, something went wrong, we don't have that information.\n\n"
                    f'Can I help you with anything else?')
         return message
 
-    elif len(the_one) == 1:
-        message = (f'{random_greet()}! {the_one[0][1]} For more information, please visit {the_one[0][2]}.\n\n'
-                   f'Can I help you with anything else?')
-        return message
-
     else:
-        random_one = random.choice(the_one)
+        random_one = random.choice(finalists)
         message = (f'{random_greet()}! {random_one[1]} For more information, please visit {random_one[2]}.\n\n'
                    f'Can I help you with anything else?')
         return message
 
 
 if __name__ == "__main__":
-    answer = answers('api contact')
+    answer = answers('Python Library')
     print()
     print(answer)
